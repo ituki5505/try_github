19cef5baef7beb99c2d7787e7a5459d646628fc5
VuDoanDungVN <vu-doan-dung-2202@occ.co.jp>
2022-05-09 16:31:18 +0900

    "Sended"

---
+++ try_github/try_github/Super.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace try_github
+{
+    internal class Super
+    {
+        public int add(int a, int b)
+        {
+           return a + b;
+        }
+    }
+}
